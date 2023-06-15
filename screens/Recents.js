f32be1952aa06404ff6d26eb29d1387d3be50f02
Koushik Mondal <darkdeavil004@gmail.com>
2023-06-15 18:23:47 +0530

    File Structure and Navigation Added

---
+++ screens/Recents.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Recents = () => {
+  return (
+    <View>
+      <Text>Recents</Text>
+    </View>
+  )
+}
+
+export default Recents
+
+const styles = StyleSheet.create({})
