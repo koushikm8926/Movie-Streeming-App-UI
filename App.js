f31f8f03e579c2d1bfdb9dab590e437c754fdf1e
Koushik Mondal <darkdeavil004@gmail.com>
2023-06-16 12:30:56 +0530

    styled tab navigation screen bar

--- App.js
+++ App.js
@@ -1,32 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import Home from './screens/Home';
-import Profile from './screens/Profile';
-import Recents from './screens/Recents';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+import Recents from "./screens/Recents";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 const Tab = createMaterialBottomTabNavigator();
-
 
 export default function App() {
   return (
-   <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Recents" component={Recents} />
-    </Tab.Navigator>
-   </NavigationContainer>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRoute="Home"
+        activeColor="#02ad94"
+        inactiveColor="#dedede"
+        barStyle={{ backgroundColor: "#0f0f0f", padding: 4 }}
+        style={{ backgroundColor: "#000" }}
+        
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={30}  />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="camera-metering-spot" color={color} size={30} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Recents"
+          component={Recents}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={30} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
